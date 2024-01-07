858cdb682e7bbc5fc2a1a6b0b2af9ac4d089a028
Nirmal <nirmalraj1025@gmail.com>
2024-01-07 17:32:02 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const dbconnection = require("./config/database");
+
+// config
+dotenv.config({ path: "server/config/config.env" });
+
+// database connection
+dbconnection();
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is working on http://localhost:${process.env.PORT}`);
+});
