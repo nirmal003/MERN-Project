5215b2a01fb64e702365e755bf099b1f1bff8b4e
Nirmal <nirmalraj1025@gmail.com>
2024-01-07 20:02:10 +0530

    basic setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("fghjk"));
+app.get("/", (req, res) =>
+  res.send(`server is working fine
+<a href="/product">product</a>`)
+);
+
+app.get("/product", (req, res) => res.send("FFF"));
 
 module.exports = app;
