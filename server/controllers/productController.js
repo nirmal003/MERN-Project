be9037573a7454f6e2a1ffd33ab096c2b303ae71
Nirmal <nirmalraj1025@gmail.com>
2024-01-08 21:54:21 +0530

    Middleware Error

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -34,40 +34,32 @@ exports.getProductDetail = createAsyncError(async (req, res, next) => {
 exports.updateProduct = createAsyncError(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
-  if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
+  if (!product) next(new ErrorHandler("Product not found", 404));
+  else {
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      product,
     });
   }
-
-  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
-
-  res.status(200).json({
-    success: true,
-    product,
-  });
 });
 
 // Delete Product -- Admin
 exports.deleteProduct = createAsyncError(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
-  if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
+  if (!product) next(new ErrorHandler("Product not found", 404));
+  else {
+    await product.deleteOne({ _id: req.params.id });
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
     });
   }
-
-  await product.deleteOne({ _id: req.params.id });
-
-  res.status(200).json({
-    success: true,
-    message: "Product deleted successfully",
-  });
 });
