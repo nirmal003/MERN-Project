449e73cc38e530aced2901ff1fdbda086a320264
Nirmal <nirmalraj1025@gmail.com>
2024-01-19 17:21:36 +0530

    Reset Password

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -14,7 +14,7 @@ router.route("/login").post(loginUser);
 
 router.route("/password/forgot").post(forgotPassword);
 
-router.route("/password/reset/:token").post(resetPassword);
+router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logoutUser);
 
