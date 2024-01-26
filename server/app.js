3ac98c1c4feff4ba14042aed3aa6d255b6636afa
Nirmal <nirmalraj1025@gmail.com>
2024-01-26 12:23:14 +0530

    Make Cloudinary Config & fileUpload

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
 const errorMiddleware = require("./middleware/error");
 
@@ -11,6 +13,8 @@ const order = require("./routes/orderRoute");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser().urlencoded({ extended: true }));
+app.use(fileUpload());
 
 app.get("/", (req, res) =>
   res.send(`server is working fine
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
+const cloudinary = require("cloudinary");
 const dbconnection = require("./config/database");
 
 // Handling Uncaught Exception Error
@@ -17,6 +18,13 @@ dotenv.config({ path: "server/config/config.env" });
 
 // database connection
 dbconnection();
+
+//  MMake Cloudinary Config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOULDINARY_API_SECRET,
+});
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`server is working on http://localhost:${process.env.PORT}`);
