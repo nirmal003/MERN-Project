da3def3382470fb196b37bd6c159e980d58070e0
Nirmal <nirmalraj1025@gmail.com>
2024-01-27 22:06:30 +0530

    Increase limit for file

--- server/app.js
+++ server/app.js
@@ -12,9 +12,9 @@ const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "40mb" }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "40mb" }));
 
 app.get("/", (req, res) =>
   res.send(`server is working fine
